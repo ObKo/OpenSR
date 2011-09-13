eedf90af87daae37bafa3e171b25fb7a1d389ee8
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2011-09-13 22:23:23 +0700

    Forgotten pkg2rpkg sources in resource converter.

---
+++ tools/ResourceConverter/pkg2rpkg.cpp
@@ -0,0 +1,83 @@
+#include "pkg2rpkg.h"
+#include <libRanger.h>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <cstring>
+#include <map>
+
+using namespace std;
+using namespace Rangers;
+
+namespace Rangers
+{
+void countFiles(std::wstring dir, PKGItem *base, std::map<RPKGEntry *, PKGItem*>& itemMap)
+{
+    for (int i = 0; i < base->childCount; i++)
+    {
+        if (base->childs[i].dataType == 3)
+            countFiles(dir + fromASCII(base->name) + L"/", &base->childs[i], itemMap);
+        else
+        {
+            RPKGEntry *e= new RPKGEntry;
+            e->name = dir + fromASCII(base->childs[i].name);
+            itemMap[e] = &base->childs[i];
+        }
+    }
+}
+
+void cleanup(PKGItem *root)
+{
+    for (int i = 0; i < root->childCount; i++)
+    {
+        if (root->childs[i].dataType == 3)
+            cleanup(&root->childs[i]);
+    }
+    delete[] root->childs;
+}
+
+int pkg2rpkg(const std::string& inputFile, const std::string& outputFile)
+{
+    ifstream pkgfile(inputFile.c_str(), ios::binary | ios::in);
+    std::map<RPKGEntry*, PKGItem*> itemMap;
+    PKGItem *root = loadPKG(pkgfile);
+    if (root->childCount != 1)
+    {
+        std::cout << "Incorrect input file" << endl;
+        return -1;
+    }
+    countFiles(L"", &root->childs[0], itemMap);
+
+    //FIXME: OH SHI~
+    std::vector<RPKGEntry> files;
+    std::vector<PKGItem> pkgfiles;
+    for (std::map<RPKGEntry*, PKGItem*>::iterator i = itemMap.begin(); i != itemMap.end(); i++)
+    {
+        files.push_back(*(i->first));
+        pkgfiles.push_back(*(i->second));
+    }
+
+    int c = itemMap.size();
+
+    uint32_t headerSize = calculateRPKGHeaderSize(files);
+    ofstream outfile(outputFile.c_str(), ios::binary | ios::out);
+    //FIXME: Better way to fill file
+    for (int i = 0; i < headerSize + 8; i++)
+        outfile.put(0);
+
+    for (int i = 0; i < files.size(); i++)
+    {
+        wcout << files[i].name << endl;
+        unsigned char *data = extractFile(pkgfiles[i], pkgfile);
+        appendRPKGFile(outfile, files[i], (const char*)data, pkgfiles[i].size);
+        delete data;
+    }
+    outfile.seekp(0, ios_base::beg);
+    writeRPKGHeader(outfile, files);
+
+    cleanup(root);
+    delete root;
+    outfile.close();
+    pkgfile.close();
+}
+};
