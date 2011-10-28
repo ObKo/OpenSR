2d95d2f63c4ef26b2324ca35b6be03b90059a73c
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2011-10-28 16:25:28 +0700

    New class Widget node for relative positioning.

---
+++ src/WidgetNode.cpp
@@ -0,0 +1,44 @@
+/*
+    OpenSR - opensource multi-genre game based upon "Space Rangers 2: Dominators"
+    Copyright (C) 2011 Kosyak <ObKo@mail.ru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "WidgetNode.h"
+
+namespace Rangers
+{
+WidgetNode::WidgetNode(Widget *parent): Widget(parent)
+{
+
+}
+
+void WidgetNode::draw() const
+{
+    if (!prepareDraw())
+            return;
+
+        for (std::list<Object*>::const_iterator i = m_children.begin(); i != m_children.end(); i++)
+            (*i)->draw();
+
+        endDraw();
+}
+
+void WidgetNode::processLogic(int dt)
+{
+    for (std::list<Object*>::const_iterator i = m_children.begin(); i != m_children.end(); i++)
+            (*i)->processLogic(dt);
+}
+}
