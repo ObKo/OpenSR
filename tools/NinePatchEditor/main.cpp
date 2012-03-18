3e5737b52a95f125f2dccf602d4520037cda847b
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2012-03-18 10:26:30 +0700

    New tool - 9patch editor.

---
+++ tools/NinePatchEditor/main.cpp
@@ -0,0 +1,13 @@
+#include <QtGui/QApplication>
+#include "EditorWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QApplication::setOrganizationName("QNinePatchEditor");
+    QApplication::setApplicationName("QNinePatchEditor");
+    QNPE::EditorWindow w;
+    w.show();
+
+    return a.exec();
+}
