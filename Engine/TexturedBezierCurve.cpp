fa3f8dec0f7cd8e77c84552e88e8242f7da580e1
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2015-09-26 20:07:21 +0600

    Textured Bezier curve.
    Fix build on new Qt.
    Improve astyle script.

---
+++ Engine/TexturedBezierCurve.cpp
@@ -0,0 +1,100 @@
+/*
+    OpenSR - opensource multi-genre game based upon "Space Rangers 2: Dominators"
+    Copyright (C) 2015 Kosyak <ObKo@mail.ru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "OpenSR/TexturedBezierCurve.h"
+
+#include <OpenSR/Engine.h>
+#include <OpenSR/ResourceManager.h>
+#include <OpenSR/QMLHelper.h>
+#include <QImageReader>
+#include <QSGSimpleTextureNode>
+#include <QQuickWindow>
+
+namespace OpenSR
+{
+class TexturedBezierCurvePrivate
+{
+    TexturedBezierCurve *q_ptr;
+    Q_DECLARE_PUBLIC(TexturedBezierCurve)
+
+    TexturedBezierCurvePrivate(TexturedBezierCurve *q);
+
+    BezierCurve curve;
+    int minStep;
+};
+
+TexturedBezierCurvePrivate::TexturedBezierCurvePrivate(TexturedBezierCurve *q)
+{
+    q_ptr = q;
+
+    curve.p0 = QPointF(0.0f, 0.0f);
+    curve.p1 = QPointF(0.0f, 0.0f);
+    curve.p2 = QPointF(0.0f, 0.0f);
+    curve.p3 = QPointF(0.0f, 0.0f);
+
+    minStep = 1;
+}
+
+TexturedBezierCurve::TexturedBezierCurve(QQuickItem* parent): TexturedPolyline(parent),
+    d_osr_ptr(new TexturedBezierCurvePrivate(this))
+{
+    Q_D(TexturedBezierCurve);
+}
+
+TexturedBezierCurve::~TexturedBezierCurve()
+{
+    Q_D(TexturedBezierCurve);
+}
+
+QVariant TexturedBezierCurve::curve() const
+{
+    Q_D(const TexturedBezierCurve);
+    return QVariant::fromValue(d->curve);
+}
+
+void TexturedBezierCurve::setCurve(const QVariant& curve)
+{
+    Q_D(TexturedBezierCurve);
+
+    d->curve = QML::QMLHelper::convertBezierCurveFromJS(curve);
+
+    QList<QPointF> points = d->curve.calcPolyline(d->minStep);
+    QVariantList vpoints;
+
+    for(const QPointF& p: points)
+        vpoints.append(QVariant(p));
+
+    setPoints(vpoints);
+    emit(curveChanged());
+}
+
+int TexturedBezierCurve::minStep() const
+{
+    Q_D(const TexturedBezierCurve);
+    return d->minStep;
+}
+
+void TexturedBezierCurve::setMinStep(int minStep)
+{
+    Q_D(TexturedBezierCurve);
+    d->minStep = minStep;
+    emit(minStepChanged());
+
+    setCurve(QML::QMLHelper::convertBezierCurveToJS(d->curve));
+}
+}
---
+++ include/OpenSR/TexturedBezierCurve.h
@@ -0,0 +1,56 @@
+/*
+    OpenSR - opensource multi-genre game based upon "Space Rangers 2: Dominators"
+    Copyright (C) 2015 Kosyak <ObKo@mail.ru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPENSR_TEXTUREDBEZIERCURVE_H
+#define OPENSR_TEXTUREDBEZIERCURVE_H
+
+#include <OpenSR/OpenSR.h>
+#include <OpenSR/Types.h>
+#include <OpenSR/TexturedPolyline.h>
+
+namespace OpenSR
+{
+class TexturedBezierCurvePrivate;
+class ENGINE_API TexturedBezierCurve: public TexturedPolyline
+{
+    Q_OBJECT
+    OPENSR_DECLARE_PRIVATE(TexturedBezierCurve)
+
+    Q_PROPERTY(QVariant curve READ curve WRITE setCurve NOTIFY curveChanged)
+    Q_PROPERTY(int minStep READ minStep WRITE setMinStep NOTIFY minStepChanged)
+
+public:
+    TexturedBezierCurve(QQuickItem * parent = 0);
+    virtual ~TexturedBezierCurve();
+
+    QVariant curve() const;
+    int minStep() const;
+
+    void setCurve(const QVariant& curve);
+    void setMinStep(int minStep);
+
+Q_SIGNALS:
+    void curveChanged();
+    void minStepChanged();
+
+protected:
+    OPENSR_DECLARE_DPOINTER(TexturedBezierCurve)
+};
+}
+
+#endif // OPENSR_TEXTUREDBEZIERCURVE_H
